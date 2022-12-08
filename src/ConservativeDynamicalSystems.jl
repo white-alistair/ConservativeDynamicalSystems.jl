079ff7ed6b2feb0f4a8363e3e80fcde0a0a651a0
Alistair White <alistair@pik-potsdam.de>
2022-12-08 11:24:41 +0100

    Add QBH

--- src/ConservativeDynamicalSystems.jl
+++ src/ConservativeDynamicalSystems.jl
@@ -5,5 +5,6 @@ abstract type AbstractDynamicalSystem{T<:AbstractFloat} end
 include("constants.jl")
 include("simple_pendulum.jl")
 include("double_pendulum.jl")
+include("qbh.jl")
 
 end
---
+++ src/qbh.jl
@@ -0,0 +1,35 @@
+struct QuadrupoleBosonHamiltonian{T} <: AbstractDynamicalSystem{T}
+    A::T
+    B::T
+    D::T
+end
+
+QuadrupoleBosonHamiltonian{T}() where {T} = QuadrupoleBosonHamiltonian{T}(1.0, 0.55, 0.4)
+
+function get_default_initial_conditions(::QuadrupoleBosonHamiltonian{T}) where {T}
+    return T[0.0, -2.5830294658973876, 1.3873470962626937, -4.743416490252585]
+end
+
+function rhs(u::AbstractVector{T}, system::QuadrupoleBosonHamiltonian{T}, t) where {T}
+    q₀, q₂, p₀, p₂ = u
+    (; A, B, D) = system
+    
+    return [
+        A * p₀,
+        A * p₂,
+        -1 * A * q₀ - 3 * B / sqrt(2) * (q₂^2 - q₀^2) - D * q₀ * (q₀^2 + q₂^2),
+        -1 * q₂ * (A + 3 * B / sqrt(2) * q₀ + D * (q₀^2 + q₂^2)),
+    ]
+end
+
+function rhs!(du::AbstractVector{T}, u::AbstractVector{T}, system::QuadrupoleBosonHamiltonian{T}, t) where {T}
+    q₀, q₂, p₀, p₂ = u
+    (; A, B, D) = system
+
+    du[1] = A * p₀
+    du[2] = A * p₂
+    du[3] = -1 * A * q₀ - 3 * B / sqrt(2) * (q₂^2 - q₀^2) - D * q₀ * (q₀^2 + q₂^2)
+    du[4] = -1 * q₂ * (A + 3 * B / sqrt(2) * q₀ + D * (q₀^2 + q₂^2))
+
+    return nothing
+end
