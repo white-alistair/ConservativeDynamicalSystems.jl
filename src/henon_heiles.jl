2429c5e1415859f6d5df10bc56a1b151985ed150
Alistair White <alistair@pik-potsdam.de>
2023-01-03 20:21:15 +0100

    Add in-place methods

--- src/henon_heiles.jl
+++ src/henon_heiles.jl
@@ -6,19 +6,19 @@ HenonHeilesSystem{T}() where {T} = HenonHeilesSystem{T}(1.0)
 
 function rhs(u::AbstractVector{T}, system::HenonHeilesSystem, t) where {T}
     (; λ) = system
-    x, y, p_x, p_y  = u
+    x, y, p_x, p_y = u
 
-    return [
-        p_x,
-        p_y,
-        -x - 2λ * x * y,
-        -y - λ * (x^2 - y^2),
-    ]
+    return [p_x, p_y, -x - 2λ * x * y, -y - λ * (x^2 - y^2)]
 end
 
-function rhs!(du::AbstractVector{T}, u::AbstractVector{T}, system::HenonHeilesSystem{T}, t) where {T}
+function rhs!(
+    du::AbstractVector{T},
+    u::AbstractVector{T},
+    system::HenonHeilesSystem{T},
+    t,
+) where {T}
     (; λ) = system
-    x, y, p_x, p_y  = u
+    x, y, p_x, p_y = u
 
     du[1] = p_x
     du[2] = p_y
@@ -30,7 +30,7 @@ end
 
 function hamiltonian(u::AbstractVector{T}, system::HenonHeilesSystem{T}, t::T) where {T}
     (; λ) = system
-    x, y, p_x, p_y  = u
+    x, y, p_x, p_y = u
     return 0.5 * (p_x^2 + p_y^2) + 0.5 * (x^2 + y^2) + λ * (x^2 * y - y^3 / 3)
 end
 
@@ -39,9 +39,40 @@ function constraints(u::AbstractVector{T}, system::HenonHeilesSystem{T}, t::T) w
     return [hamiltonian(u, system, t)]
 end
 
-function constraints_jacobian(u::AbstractVector{T}, system::HenonHeilesSystem{T}, t::T) where {T}
+function constraints!(
+    constraints::AbstractVector{T},
+    u::AbstractVector{T},
+    system::DoublePendulum{T},
+    t::T,
+) where {T}
+    constraints[1] = hamiltonian(u, system, t)
+    return nothing
+end
+
+function constraints_jacobian(
+    u::AbstractVector{T},
+    system::HenonHeilesSystem{T},
+    t::T,
+) where {T}
     (; λ) = system
-    x, y, p_x, p_y  = u
-    
+    x, y, p_x, p_y = u
+
     return [x + 2λ * y;; y + λ * (x^2 - y^2);; p_x;; p_y]
+end
+
+function constraints_jacobian!(
+    J::AbstractMatrix{T},
+    u::AbstractVector{T},
+    system::HenonHeilesSystem{T},
+    t::T,
+) where {T}
+    (; λ) = system
+    x, y, p_x, p_y = u
+
+    J[1, 1] = x + 2λ * y
+    J[1, 2] = y + λ * (x^2 - y^2)
+    J[1, 3] = p_x
+    J[1, 4] = p_y
+
+    return nothing
 end
