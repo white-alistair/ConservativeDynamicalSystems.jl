aa0b14ef002857284308a1e9a10f5c6c89f731f0
Alistair White <alistair@pik-potsdam.de>
2023-01-03 20:31:36 +0100

    Add tests

--- test/double_pendulum.jl
+++ test/double_pendulum.jl
@@ -8,11 +8,24 @@
         system = DoublePendulum{T}(rand(T, 4)...)
         u0 = rand(T, 4)
         t0 = zero(T)
-        tspan = (t0, T(10.0)) 
-        initial_constraints = constraints(u0, system, t0)
-        prob = ODEProblem(rhs!, u0, tspan, system)
-        sol = solve(prob, alg = Vern9(), abstol = 1e-12, reltol = 1e-12)
-        final_constraints = constraints(sol.u[end], system, sol.t[end])
-        @test initial_constraints ≈ final_constraints
+        tspan = (t0, T(10.0))
+
+        @testset "In-place" begin
+            constraints = zeros(T, 1)
+            constraints!(constraints, u0, system, t0)
+            initial_constraints = constraints
+            prob = ODEProblem(rhs!, u0, tspan, system)
+            sol = solve(prob, alg = Vern9(), abstol = 1e-12, reltol = 1e-12)
+            constraints!(constraints, sol.u[end], system, sol.t[end])
+            @test initial_constraints ≈ constraints
+        end
+
+        @testset "Out-of-place" begin
+            initial_constraints = constraints(u0, system, t0)
+            prob = ODEProblem(rhs, u0, tspan, system)
+            sol = solve(prob, alg = Vern9(), abstol = 1e-12, reltol = 1e-12)
+            final_constraints = constraints(sol.u[end], system, sol.t[end])
+            @test initial_constraints ≈ final_constraints
+        end
     end 
 end
