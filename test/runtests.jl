a5b062a668f66e9ce6922e8407a3f771075230ff
Alistair White <alistair@pik-potsdam.de>
2023-01-03 14:43:15 +0100

    Add tests for vector products

--- test/runtests.jl
+++ test/runtests.jl
@@ -1,5 +1,6 @@
 using ConservativeDynamicalSystems, Random, OrdinaryDiffEq, Test
 
 @testset "ConservativeDynamicalSystems.jl" begin
+    include("vector_products.jl")
     include("three_body_problem.jl")
 end
