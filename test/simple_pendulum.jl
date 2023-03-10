942b57196ab4a9aaad0471881dc3867d866fc857
Alistair White <alistair@pik-potsdam.de>
2023-03-10 14:00:05 +0100

    Update simple pendulum tests

--- test/simple_pendulum.jl
+++ test/simple_pendulum.jl
@@ -1,31 +1,39 @@
-@testset "simple_pendulum.jl" begin
-    @testset "Dynamics and Invariants" begin
-        # We indirectly test both the dynamics and the constraints by integrating the 
-        # system forward in time and verifying that the integrals of motion are indeed
-        # conserved.
-        Random.seed!(1)
-        T = Float64
-        system = SimplePendulum{T}(rand(T, 2)...)
-        u0 = rand(T, 2)
-        t0 = zero(T)
-        tspan = (t0, T(10.0))
+# We indirectly test both the dynamics and the constraints by integrating the 
+# system forward in time and verifying that the integrals of motion are indeed
+# conserved.
 
-        @testset "In-place" begin
-            invariants = zeros(T, 1)
-            invariants!(invariants, u0, system, t0)
-            initial_invariants = copy(invariants)
-            prob = ODEProblem(rhs!, u0, tspan, system)
-            sol = solve(prob, alg = Vern9(), abstol = 1e-12, reltol = 1e-12)
-            invariants!(invariants, sol.u[end], system, sol.t[end])
-            @test initial_invariants ≈ invariants
-        end
+@testitem "Dynamics and Invariants: in-place" tags=[:skip] begin
+    using OrdinaryDiffEq, Random
 
-        @testset "Out-of-place" begin
-            initial_invariants = invariants(u0, system, t0)
-            prob = ODEProblem(rhs, u0, tspan, system)
-            sol = solve(prob, alg = Vern9(), abstol = 1e-12, reltol = 1e-12)
-            final_invariants = invariants(sol.u[end], system, sol.t[end])
-            @test initial_invariants ≈ final_invariants
-        end
-    end 
+    Random.seed!(1)
+    T = Float64
+    system = SimplePendulum{T}(rand(T, 2)...)
+    u0 = rand(T, 2)
+    t0 = zero(T)
+    tspan = (t0, T(10.0))
+
+    invariants = zeros(T, 1)
+    invariants!(invariants, u0, system, t0)
+    initial_invariants = copy(invariants)
+    prob = ODEProblem(rhs!, u0, tspan, system)
+    sol = solve(prob; alg = Vern9(), abstol = 1e-12, reltol = 1e-12)
+    invariants!(invariants, sol.u[end], system, sol.t[end])
+    @test initial_invariants ≈ invariants
+end
+
+@testitem "Dynamics and Invariants: out-of-place" tags=[:skip] begin
+    using OrdinaryDiffEq, Random
+
+    Random.seed!(1)
+    T = Float64
+    system = SimplePendulum{T}(rand(T, 2)...)
+    u0 = rand(T, 2)
+    t0 = zero(T)
+    tspan = (t0, T(10.0))
+
+    initial_invariants = invariants(u0, system, t0)
+    prob = ODEProblem(rhs, u0, tspan, system)
+    sol = solve(prob; alg = Vern9(), abstol = 1e-12, reltol = 1e-12)
+    final_invariants = invariants(sol.u[end], system, sol.t[end])
+    @test initial_invariants ≈ final_invariants
 end
