aa88bae69463c59d82d9bf3604907717b54b81aa
Alistair White <alistair@pik-potsdam.de>
2023-03-30 12:54:55 +0200

    Fix simple pendulum code

--- src/simple_pendulum.jl
+++ src/simple_pendulum.jl
@@ -6,25 +6,55 @@ end
 SimplePendulum{T}() where {T} = SimplePendulum{T}(1.0, 1.0)
 
 function get_default_initial_conditions(::SimplePendulum{T}) where {T}
-    return T[π / 4, 0.0]
+    return T[π/4, 0.0]
 end
 
 function rhs(u::AbstractVector{T}, system::SimplePendulum, t) where {T}
     θ, ω = u
-    (; m, l) = system
-    return [ω, -g(T) / l * sin(θ)]
+    (; l) = system
+    return [ω, -1 / l * sin(θ)]
+end
+
+function (system::SimplePendulum)(u, p, t)
+    return rhs(u, system, t)
 end
 
 function rhs_jacobian(u::AbstractVector{T}, system::SimplePendulum{T}, t) where {T}
     return ForwardDiff.jacobian(u -> rhs(u, system, nothing), u)
 end
 
-function rhs!(du::AbstractVector{T}, u::AbstractVector{T}, system::SimplePendulum{T}, t) where {T}
+function rhs!(
+    du::AbstractVector{T},
+    u::AbstractVector{T},
+    system::SimplePendulum{T},
+    t,
+) where {T}
     θ, ω = u
-    (; m, l) = system
+    (; l) = system
 
     du[1] = ω
-    du[2] = -g(T) / l * sin(θ)
+    du[2] = -1 / l * sin(θ)
 
     return nothing
+end
+
+function (system::SimplePendulum)(du, u, p, t)
+    rhs!(du, u, system, t)
+    return nothing
+end
+
+function energy(u::AbstractVector, system::SimplePendulum{T}, t) where {T}
+    θ, ω = u
+    (; m, l) = system
+    return T(0.5) * m * l^2 * ω^2 - m * l * cos(θ)
+end
+
+function invariants(u, system::SimplePendulum, t)
+    return [energy(u, system, t)]
+end
+
+function invariants_jacobian(u, system::SimplePendulum, t)
+    θ, ω = u
+    (; m, l) = system 
+    return [m * l * sin(θ);; m * l^2 * ω]
 end
--- test/simple_pendulum.jl
+++ test/simple_pendulum.jl
@@ -21,7 +21,7 @@
     @test initial_invariants ≈ invariants
 end
 
-@testitem "Dynamics and Invariants: out-of-place" tags=[:skip] begin
+@testitem "Dynamics and Invariants: out-of-place" begin
     using OrdinaryDiffEq, Random
 
     Random.seed!(1)
@@ -36,4 +36,18 @@ end
     sol = solve(prob; alg = Vern9(), abstol = 1e-12, reltol = 1e-12)
     final_invariants = invariants(sol.u[end], system, sol.t[end])
     @test initial_invariants ≈ final_invariants
+end
+
+@testitem "Invariants Jacobian" begin
+    using ForwardDiff, Random
+
+    Random.seed!(1)
+    T = Float64
+    system = SimplePendulum{T}(rand(T, 2)...)
+    u = rand(T, 2)
+
+    jac_forward_diff = ForwardDiff.jacobian(u -> invariants(u, system, nothing), u)
+    jac_analytic = invariants_jacobian(u, system, nothing)
+
+    @test jac_analytic ≈ jac_forward_diff atol = 1e-5
 end
